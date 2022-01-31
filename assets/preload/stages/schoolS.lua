9fb464f4b0e60674dcd8ae1b260ea73b144c970f
ErrNatsuki1666 <96864066+BkYuris@users.noreply.github.com>
2022-01-31 13:56:36 +0800

    Add files via upload

---
+++ assets/preload/stages/schoolS.lua
@@ -0,0 +1,60 @@
+function onCreate()
+
+    -- background shit
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-pixel-dead');
+    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver-pixel');
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'bf-dies');
+    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd-pixel');
+
+    makeLuaSprite('skyd', 'weeb/weebSky', 300, 240);
+	addLuaSprite('skyd', false);
+    setProperty('skyd.visible', true)
+	scaleObject('skyd', 7, 7);
+	setLuaSpriteScrollFactor('skyd', 0.9, 0.9);
+    
+    makeLuaSprite('backschoold', 'weeb/weebSchool', 80, 180);
+	addLuaSprite('backschoold', false);
+    setProperty('backschoold.visible', true)
+	scaleObject('backschoold', 7, 7);
+	setLuaSpriteScrollFactor('backschoold', 0.7, 1);
+
+    makeLuaSprite('streetd', 'weeb/weebStreet', 1, 1);
+	addLuaSprite('streetd', false);
+	scaleObject('streetd', 8.5, 8.5);
+    setProperty('streetd.visible', true)
+	setLuaSpriteScrollFactor('streetd', 1, 1);
+
+	makeLuaSprite('tresssd', 'weeb/weebTreesBack', 450, 325);
+	addLuaSprite('tresssd', false);
+    setProperty('tresssd.visible', true)
+	scaleObject('tresssd', 5.5, 5.5);
+	setLuaSpriteScrollFactor('weeb/tresssd', 1, 1);
+
+	makeAnimatedLuaSprite('aaaad', 'weeb/weebTrees', 1, 220);
+	addAnimationByPrefix('aaaad', 'bruhD', 'TreesD', 10, true);
+	setGraphicSize('aaaad', getProperty('aaaad.width') * 8.5)
+	setProperty('aaaad.visible', true);
+	addLuaSprite('aaaad', false);
+
+	makeAnimatedLuaSprite('petal1', 'weeb/petals', 1, 220);
+	addAnimationByPrefix('petal1', 'bruuhh1', 'PETALS ALL0', 24, true);
+	objectPlayAnimation('petal1', 'bruuhh1', true);
+	setGraphicSize('petal1', getProperty('petal1.width') * 8.5)
+	addLuaSprite('petal1', false);
+	setObjectCamera('petal1', 'game');
+
+	setProperty('skyd.antialiasing', false)
+	setProperty('backschoold.antialiasing', false)
+	setProperty('streetd.antialiasing', false)
+	setProperty('tresssd.antialiasing', false)
+	setProperty('aaaad.antialiasing', false)
+	setProperty('freaks.antialiasing', false)
+	setProperty('petal1.antialiasing', false)
+
+end
+
+function onBeatHit()
+
+	objectPlayAnimation('aaaad', 'TreesD', true);
+
+end
