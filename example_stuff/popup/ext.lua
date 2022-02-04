b0233914543b688500bfc226b7fc1b5dde1610ab
ErrNatsuki1666 <96864066+BkYuris@users.noreply.github.com>
2022-02-04 12:55:54 +0800

    Add files via upload

---
+++ example_stuff/popup/ext.lua
@@ -0,0 +1,17 @@
+local path = "WebExtension.xml";
+
+do path == fileDirectory;
+
+number = 355;
+
+local lim = {
+number "355"
+do number -1 end
+limit number "1";
+}
+--ex. 355 - 1 = 354
+
+if curSteps = {lim} 
+end
+
+if curStepsLua = {"1", +1}
